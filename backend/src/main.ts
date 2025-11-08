eaa347325f50cf1b78a1aba69fc2eae5fc3e38a7
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2025-11-08 13:17:05 -0300

    feat: enhance Swagger API documentation setup with custom site title, favicon, and updated styles for improved user experience

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -39,7 +39,19 @@ async function bootstrap() {
 		.build();
 
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api/docs', app, document);
+	SwaggerModule.setup('api/docs', app, document, {
+		customSiteTitle: 'Altaa.ai API',
+		customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+		customJs: [
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+		],
+		customCssUrl: [
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+		],
+	});
 
 	app.enableCors({
 		origin: true,
